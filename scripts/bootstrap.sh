219c2aa89a5d85be1d33e352e929e79c487ad530
Alok Kumar <rajalokan@gmail.com>
2019-08-01 22:39:46 +0530

    working minikube

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -33,6 +33,8 @@ ctx logger info "Installing Minikube"
 curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /tmp/minikube-linux-amd64
 sudo install /tmp/minikube-linux-amd64 /usr/bin/minikube
 
+minikube start
+
 # # Start minikube
 ctx logger info "Starting Minikube"
 # export MINIKUBE_WANTUPDATENOTIFICATION=false
@@ -43,28 +45,62 @@ mkdir -p $HOME/.kube || true
 touch $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
 
-# Fix in kubeadm
-echo '1' | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
+# # Fix in kubeadm
+# echo '1' | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
 
 # Start minikube
 sudo -E minikube start --vm-driver=none
-sudo -E minikube addons enable dashboard
+# sudo -E minikube addons enable dashboard
 
-kubectl run hello-minikube --image=k8s.gcr.io/echoserver:1.10 --port=8080
-kubectl expose deployment hello-minikube --type=NodePort
-kubectl get pod
-minikube service hello-minikube --url
-curl $(minikube service hello-minikube --url)
+minikube status
+kubectl cluster-info
 
-sudo chown -R $USER $HOME/.minikube
+mkdir -p ~/k8s/nginx
+cat <<EOM >~/k8s/nginx/deployment.yaml
+apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
+kind: Deployment
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 2 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.7.9
+        ports:
+        - containerPort: 80
+EOM
 
-ctx logger info "Bootstrap Successful"
+kubectl apply -f deployment.yaml
+kubectl get deployments
+kubectl describe deployment nginx-deployment
+kubectl get pods -l app=nginx
+# kubectl describe pod <pod-name>
 
-# sudo mv /root/.kube $HOME/.kube \
-#     && sudo chown -R $USER $HOME/.kube \
-#     && sudo chgrp -R $USER $HOME/.kube
-# sudo mv /root/.minikube $HOME/.minikube \
+
+
+# kubectl run hello-minikube --image=k8s.gcr.io/echoserver:1.10 --port=8080
+# kubectl expose deployment hello-minikube --type=NodePort
+# kubectl get pod
+# minikube service hello-minikube --url
+# curl $(minikube service hello-minikube --url)
 #
-#     && sudo chgrp -R $USER $HOME/.minikube
-
-# sudo chown -R $USER: $HOME/.minikube
+# sudo chown -R $USER $HOME/.minikube
+#
+# ctx logger info "Bootstrap Successful"
+#
+# # sudo mv /root/.kube $HOME/.kube \
+# #     && sudo chown -R $USER $HOME/.kube \
+# #     && sudo chgrp -R $USER $HOME/.kube
+# # sudo mv /root/.minikube $HOME/.minikube \
+# #
+# #     && sudo chgrp -R $USER $HOME/.minikube
+#
+# # sudo chown -R $USER: $HOME/.minikube
